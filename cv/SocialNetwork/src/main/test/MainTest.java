2bb6d313b4d8824bdbbd08c445b1aadf24592378
Vojtěch Kropáček <kropacekv@btlnet.com>
2023-01-01 22:01:21 +0100

    tests

--- cv/SocialNetwork/src/main/test/MainTest.java
+++ cv/SocialNetwork/src/main/test/MainTest.java
@@ -3,6 +3,8 @@ package SocialNetwork.src.main.test;
 import SocialNetwork.src.main.java.Group;
 import SocialNetwork.src.main.java.SocialNetwork;
 import SocialNetwork.src.main.java.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
@@ -10,7 +12,7 @@ public class MainTest {
     @Test
     public void testFeed(){
         SocialNetwork facebook = new SocialNetwork();
-        Group pejkari = new Group("pejkari");
+        Group pejkari = new Group("pejskari");
         Group kockari = new Group("Kockari");
         facebook.registerGroup(pejkari);
         facebook.registerGroup(kockari);
@@ -33,13 +35,23 @@ public class MainTest {
         kockari.AddMember(u1);
         u3.makePost("nemam rad kocky");
         pejkari.PostToGroup(u3, "mam damlatyna");
-        u1.makePost("kiki je krava");
+        u1.makePost("kiki je blba");
         kockari.PostToGroup(u2, "mam zrzavou");
         String u1feed = u1.printFeed();
+        Assertions.assertTrue(u1feed.contains("u3") && u1feed.contains("u1") && u1feed.contains("Kockari") && u1feed.contains("u2"));
+        Assertions.assertFalse(u1feed.contains("u4") ||  u1feed.contains("pejkari"));
         String u2feed = u2.printFeed();
+        Assertions.assertTrue(u2feed.contains("u3") && u2feed.contains("u1") && u2feed.contains("Kockari") && u2feed.contains("u2"));
+        Assertions.assertFalse(u2feed.contains("u4") || u2feed.contains("pejkari"));
         String u3feed = u3.printFeed();
+        Assertions.assertTrue(u3feed.contains("u3") && u3feed.contains("u1") && u3feed.contains("pejskari"));
+        Assertions.assertFalse(u3feed.contains("u4") || u3feed.contains("u2") || u3feed.contains("Kockari"));
         String u4feed = u4.printFeed();
+        Assertions.assertTrue( u4feed.contains("u1") && u4feed.contains("pejskari") && u4feed.contains("u3"));
+        Assertions.assertFalse(u4feed.contains("u4") || u4feed.contains("u2") || u4feed.contains("Kockari"));
         String pejskarifeed = pejkari.PrintFeed();
+        Assertions.assertFalse(pejskarifeed.contains("u4") || pejskarifeed.contains("u2") || pejskarifeed.contains("u1"));
         String kockarifeed = kockari.PrintFeed();
+        Assertions.assertFalse(kockarifeed.contains("u4") || kockarifeed.contains("u3") || kockarifeed.contains("u1"));
     }
 }
